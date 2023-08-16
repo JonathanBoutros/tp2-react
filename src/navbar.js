91d52511cb4e59f1ca68f35587db948d34341f90
JonathanBoutros <boutrosjo123@gmail.com>
2023-08-16 10:34:18 -0400

    debugage navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,34 +1,43 @@
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import './css/navbar.css';
 
 const Nav = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-danger">
-            <a className="navbar-brand" href="#">Roasted cup</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav ">
-                    <li className="nav-item">
-                        <Link to={"/cafe"} className="nav-link">Café</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/"} className="nav-link">Accueil</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/cart"} className="nav-link">Cart</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/wishlist"} className="nav-link">Wishlist</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/historique"} className="nav-link">Historique</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-danger">
+      <Link to={"/"} className="navbar-brand">RoastedCup</Link>       
+      <button
+        className={`navbar-toggler ${menuOpen ? '' : 'collapsed'}`}
+        type="button"
+        onClick={toggleMenu}
+        aria-expanded={menuOpen}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarNav">
+        <ul className="navbar-nav mr-2">
+            <li className="nav-item" style={{ alignSelf: 'flex-end' }}>
+                <Link to={"/cafe"} className="nav-link">Café</Link>
+            </li>
+            <li className="nav-item" style={{ alignSelf: 'flex-end' }}>
+                <Link to={"/cart"} className="nav-link">Cart</Link>
+            </li>
+            <li className="nav-item" style={{ alignSelf: 'flex-end' }}>
+                <Link to={"/wishlist"} className="nav-link">Wishlist</Link>
+            </li>
+            <li className="nav-item" style={{ alignSelf: 'flex-end' }}>
+                <Link to={"/historique"} className="nav-link">Historique</Link>
+            </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
