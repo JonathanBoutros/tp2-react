ddd80895ce7667adc299a73b3ddb0df9fb4e917d
DominikProvencher <deftonesdom@gmail.com>
2023-08-24 12:41:02 -0400

    Ajout du lien pour le fichier css

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,59 +1,60 @@
 import React, { useState, useEffect } from 'react';
+import './css/wishlist.css';
 
 const Wishlist = () => {
-    const [wishlists, setWishlists] = useState([]);
+  const [wishlists, setWishlists] = useState([]);
 
-    useEffect(() => {
-        fetch('https://insta-api-api.0vxq7h.easypanel.host/wishlist')
-        .then((response) => response.json())
-        .then((data) => setWishlists(data))
-        .catch((error) => console.error('Error fetch:', error));
-    }, []);
+  useEffect(() => {
+    fetch('https://insta-api-api.0vxq7h.easypanel.host/wishlist')
+      .then((response) => response.json())
+      .then((data) => setWishlists(data))
+      .catch((error) => console.error('Error fetch:', error));
+  }, []);
 
-    const removeFromWish = async (productId) => {
-        try {
-          const response = await fetch(`https://insta-api-api.0vxq7h.easypanel.host/wishlist/delete-product/${productId}`, {
-            method: 'DELETE',
-          });
-    
-          console.log('Cart item deleted:', response);
-    
-          window.location.reload();
-        } catch (error) {
-          console.error('Error delete product cart:', error);
-        }
-      };
-    
-      const removeAllWish = async () => {
-        try {
-          const response = await fetch('https://insta-api-api.0vxq7h.easypanel.host/wishlist/clear', {
-            method: 'DELETE',
-          });
-    
-          console.log('All cart item deleted:', response);
-    
-          window.location.reload();
-        } catch (error) {
-          console.error('Error delete all product cart:', error);
-        }
-      };
+  const removeFromWish = async (productId) => {
+    try {
+      const response = await fetch(`https://insta-api-api.0vxq7h.easypanel.host/wishlist/delete-product/${productId}`, {
+        method: 'DELETE',
+      });
 
-    return (
-        <div>
-            <h1>Wishlist</h1>
-            <div>
-            <button onClick={removeAllWish}>Tout Effacer</button>
-            {wishlists.map((wishlist) => (
-                <div key={wishlist.id}>
-                <img src={wishlist.image} alt={wishlist.name} />
-                <p>name: {wishlist.name}</p>
-                <p>Prix: {wishlist.price}$</p>
-                <button onClick={() => removeFromWish(wishlist.id)}>Effacer</button>
-                </div>
-            ))}
-            </div>
-        </div>
-    );
+      console.log('Cart item deleted:', response);
+
+      window.location.reload();
+    } catch (error) {
+      console.error('Error delete product cart:', error);
+    }
+  };
+
+  const removeAllWish = async () => {
+    try {
+      const response = await fetch('https://insta-api-api.0vxq7h.easypanel.host/wishlist/clear', {
+        method: 'DELETE',
+      });
+
+      console.log('All cart item deleted:', response);
+
+      window.location.reload();
+    } catch (error) {
+      console.error('Error delete all product cart:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Wishlist</h1>
+      <div>
+        <button onClick={removeAllWish}>Tout Effacer</button>
+        {wishlists.map((wishlist) => (
+          <div key={wishlist.id}>
+            <img src={wishlist.image} alt={wishlist.name} />
+            <p>name: {wishlist.name}</p>
+            <p>Prix: {wishlist.price}$</p>
+            <button onClick={() => removeFromWish(wishlist.id)}>Effacer</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Wishlist;
